a17cbc881afcb5eb126c6701a4b8bef1e86b6485
Song He <1311471185@qq.com>
2024-03-16 03:19:35 +0800

    333

--- major/Week1/LinkList/duLinkList/Sources/main.c
+++ major/Week1/LinkList/duLinkList/Sources/main.c
@@ -1,5 +1,10 @@
 //
 // Created by 何松 on 2024/3/16.
 //
-#include "linkedList.h"
-#include "stdio.h"
+#include <stdio.h>
+#include "../Headers/duLinkedList.h"
+
+int main(){
+    printf("1");
+    return 0;
+}
--- minor/Week1/ddasd.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by 何松 on 2024/3/16.
-//
-
-#include "ddasd.h"
